755d01acaa2576cb367fbdf72020338dc0055ac9
takumma <taku.3001@icloud.com>
2023-08-11 20:50:09 +0900

    fix

--- test/step1.c
+++ test/step1.c
@@ -31,6 +31,10 @@ int main (int argc, char *argv[])
         errorf("dummy_init() failure");
         return -1;
     }
+    if (net_run() == -1) {
+        errorf("net_run() failure");
+        return -1;
+    }
     while (!terminate) {
         if (net_device_output(dev, 0x0800, test_data, sizeof(test_data), NULL) == -1) {
             errorf("net_device_output() failure");
