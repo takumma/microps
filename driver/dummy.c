164f5666910376e6c4a208826e2fd5b8afece325
takumma <taku.3001@icloud.com>
2023-08-12 08:38:39 +0900

    impl

--- driver/dummy.c
+++ driver/dummy.c
@@ -2,16 +2,27 @@
 #include <stddef.h>
 #include <stdint.h>
 
+#include "platform.h"
+
 #include "util.h"
 #include "net.h"
 
 #define DUMMY_MTU UINT16_MAX /* maximum size of IP datagram */
+
+#define DUMMY_IRQ INTR_IRQ_BASE
 
 static int dummy_transmit(struct net_device *dev, uint16_t type, const uint8_t *data, size_t len, const void *dst)
 {
     debugf("dev=%s, type-0x%04x, len=%zu", dev->name, type, len);
     debugdump(data, len);
     /* drop data */
+    intr_raise_irq(DUMMY_IRQ);
+    return 0;
+}
+
+static int dummy_isr(unsigned int irq, void *id)
+{
+    debugf("irq=%u, dev=%s", irq, ((struct net_device *)id)->name);
     return 0;
 }
 
@@ -19,7 +30,7 @@ static struct net_device_ops dummy_ops = {
     .transmit = dummy_transmit,
 };
 
-struct net_device * dummy_init(void)
+struct net_device *dummy_init(void)
 {
     struct net_device *dev;
 
@@ -37,6 +48,7 @@ struct net_device * dummy_init(void)
         errorf("net_device_register() failure");
         return NULL;
     }
+    intr_request_irq(DUMMY_IRQ, dummy_isr, INTR_IRQ_SHARED, dev->name, dev);
     debugf("initialized, dev=%s", dev->name);
     return dev;
 }
