3508f5ba93732b96167fb3a458cbb21ca8676b99
takumma <taku.3001@icloud.com>
2023-08-13 21:35:50 +0900

    fix test

--- test/step5.c
+++ test/step5.c
@@ -37,7 +37,7 @@ int main (int argc, char *argv[])
         return -1;
     }
     while (!terminate) {
-        if (net_device_output(dev, 0x0800, test_data, sizeof(test_data), NULL) == -1) {
+        if (net_device_output(dev, NET_PROTOCOL_TYPE_IP, test_data, sizeof(test_data), NULL) == -1) {
             errorf("net_device_output() failure");
             break;
         }
