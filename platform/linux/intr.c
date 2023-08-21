1ff68201c7424e7a36d6387b1c94003e7f1535ae
takumma <taku.3001@icloud.com>
2023-08-21 20:29:04 +0900

    step16

--- platform/linux/intr.c
+++ platform/linux/intr.c
@@ -2,7 +2,8 @@
 #include <string.h>
 #include <signal.h>
 #include <pthread.h>
-#include <sys/signal.h>
+#include <time.h>
+#include <errno.h>
 
 #include "platform.h"
 
@@ -63,13 +64,34 @@ int intr_raise_irq(unsigned int irq)
     return pthread_kill(tid, (int)irq);
 }
 
+static int intr_timer_setup(struct itimerspec *interval)
+{
+    timer_t id;
+
+    if (timer_create(CLOCK_REALTIME, NULL, &id) == -1) {
+        errorf("timer_create: %s", strerror(errno));
+        return -1;
+    }
+    if (timer_settime(id, 0, interval, NULL) == -1) {
+        errorf("timer_create: %s", strerror(errno));
+        return -1;
+    }
+    return 0;
+}
+
 static void *intr_thread(void *arg)
 {
     int terminate = 0, sig, err;
     struct irq_entry *entry;
+    const struct timespec ts = {0, 1000000}; /* 1ms */
+    struct itimerspec interval = {ts, ts};
 
     debugf("start...");
     pthread_barrier_wait(&barrier);
+    if (intr_timer_setup(&interval) == -1) {
+        errorf("intr_timer_setup() failure");
+        return NULL;
+    }
     while (!terminate) {
         err = sigwait(&sigmask, &sig);
         if (err) {
@@ -79,6 +101,9 @@ static void *intr_thread(void *arg)
         switch (sig) {
             case SIGHUP:
                 terminate = 1;
+                break;
+            case SIGALRM:
+                net_timer_handler();
                 break;
             case SIGUSR1:
                 net_softirq_handler();
@@ -131,6 +156,7 @@ int intr_init(void)
     pthread_barrier_init(&barrier, NULL, 2);
     sigemptyset(&sigmask);
     sigaddset(&sigmask, SIGHUP);
+    sigaddset(&sigmask, SIGALRM);
     sigaddset(&sigmask, SIGUSR1);
     return 0;
 }
