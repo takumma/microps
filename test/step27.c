f0709cf35873987af2a4adf02b68bbad954efc2a
takumma <taku.3001@icloud.com>
2023-08-31 20:45:18 +0900

    fix test

--- test/step27.c
+++ test/step27.c
@@ -94,7 +94,7 @@ int main (int argc, char *argv[])
     }
     ip_endpoint_pton("192.0.2.2:7", &local);
     ip_endpoint_pton("192.0.2.1:10007", &foreign);
-    soc = tcp_open_rfc793(&local, NULL, 1);
+    soc = tcp_open_rfc793(&local, &foreign, 1);
     if (soc == -1) {
         errorf("tcp_open_rfc793() failure");
         return -1;
